c950354893393a7243e809121de0bd345c1911d2
Troy Dorman <troydorman@gmail.com>
2021-06-24 07:59:08 -0600

    defining vars. dom element reference vars too

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ const questions = [
   { question: "Create A Password", type: "password" },
 ];
 
+// transition times
+const shakeTime = 100; // shake transition time
+const switchTime = 200; // transition between questions
+
+// init position at first question
+let position = 0;
+
+// init dom elements
+const formBox = document.querySelector("#form-box");
+const nextBtn = document.querySelector("#next-btn");
+const prevBtn = document.querySelector("#prev-btn");
+const inputGroup = document.querySelector("#input-group");
+const inputField = document.querySelector("#input-field");
+const inputLabel = document.querySelector("#input-label");
+const inputProgress = document.querySelector("#input-progress");
+const progress = document.querySelector("#progress-bar");
+
